aa4fc6cc42fd3a699efc4e7c4179bcdeba7dfe69
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-05 14:37:33 +0200

    fix: fix register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,4 +10,4 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "email", "role",)
+        fields = UserCreationForm.Meta.fields + ("email", "role",)
